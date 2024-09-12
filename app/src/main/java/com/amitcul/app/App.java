a4de3bbe15796d59d7c910cc1b47843bb03abe28
AlexandruMitcul <alexandru.mitcul@outlook.com>
2024-09-12 17:21:32 +0100

    Created a test program example

--- app/src/main/java/com/amitcul/app/App.java
+++ app/src/main/java/com/amitcul/app/App.java
@@ -6,58 +6,67 @@ import com.amitcul.analyze.Analyzer;
 import com.amitcul.analyze.AnalyzerBuilder;
 import com.amitcul.analyze.LowerCaseFilter;
 import com.amitcul.analyze.SimpleTokenizer;
-import com.amitcul.files.DirectoryProcessingException;
 import com.amitcul.files.FileManager;
 
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class App {
     private static final Logger LOG = Logger.getLogger(App.class.getName());
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        FileManager fileManager = new FileManager();
+    public static void main(String[] args) throws IOException {
 
-        try {
-            fileManager.addDirectory("/root/jb_test_assessment/app/src/main/resources");
-        } catch (DirectoryProcessingException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-        }
-
-        try {
-            fileManager.addFile("/root/jb_test_assessment/app/src/main/resources/dirNotToInclude/from_not_to_include_dir.txt");
-        } catch (NoSuchFileException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-        }
-
-        fileManager.getFiles().forEach(System.out::println);
-
+        // Setup analyzer
         Analyzer analyzer = new AnalyzerBuilder()
                 .setTokenizer(new SimpleTokenizer())
                 .addFilter(new LowerCaseFilter())
                 .build();
 
+        FileManager fileManager = new FileManager();
+        Scanner scanner = new Scanner(System.in);
+
+        // Add files to index
+        while (true) {
+            System.out.println("Enter the absolute path of directory or file (enter empty line to proceed): ");
+            String path = scanner.nextLine();
+            if (path.isBlank()) break;
+            try {
+                fileManager.addInstance(path);
+            } catch (NoSuchFileException e) {
+                LOG.log(Level.SEVERE, "No such file or directory: {0}", e.getLocalizedMessage());
+            }
+        }
+
+        // Create an index manager
         IndexManager indexManager = new IndexManager(fileManager, analyzer);
         try {
             indexManager.index();
         } catch (IndexCreationException e) {
-            throw new RuntimeException(e);
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return;
         }
 
+        while (true) {
+            System.out.println("Enter word to query (enter empty line to proceed): ");
+            String token = scanner.nextLine();
+            if (token.isBlank()) break;
 
-        System.out.print("Found at: ");
-        indexManager.query("hello").forEach(System.out::println);
+            Set<Path> result = indexManager.query(token);
+            if (result.isEmpty()) {
+                System.out.println("There is no matches");
+            } else {
+                System.out.println("There is matches in following files:");
+                result.forEach(System.out::println);
+            }
+        }
 
-        System.out.println("woke");
-
-        System.out.print("Found at: ");
-        indexManager.query("hello").forEach(System.out::println);
-
-        while (true) {}
-
-//        indexManager.close();
+        scanner.close();
+        indexManager.close();
     }
 
 }
